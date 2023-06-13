4128679018ba07a37c1288c486cdb83628f8d272
Stan Lemon <86314+stanlemon@users.noreply.github.com>
2023-06-13 19:38:22 -0400

    Add simple test to trigger export

--- SwiftUIExamplesUITests/SwiftUIExamplesUITests.swift
+++ SwiftUIExamplesUITests/SwiftUIExamplesUITests.swift
@@ -22,20 +22,21 @@ final class SwiftUIExamplesUITests: XCTestCase {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
   }
   
-  func testExample() throws {
+  func testExport() throws {
     // UI tests must launch the application that they test.
     let app = XCUIApplication()
     app.launch()
-    
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-  }
-  
-  func testLaunchPerformance() throws {
-    if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-      // This measures how long it takes to launch your application.
-      measure(metrics: [XCTApplicationLaunchMetric()]) {
-        XCUIApplication().launch()
-      }
-    }
+
+    let fileExporterTab = app.tabBars.firstMatch
+    XCTAssertTrue(fileExporterTab.waitForExistence(timeout: 5))
+    fileExporterTab.tap()
+
+    let fileExportButton = app.buttons["Export Items"]
+    XCTAssertTrue(fileExportButton.waitForExistence(timeout: 5))
+    fileExportButton.tap()
+
+    let moveButton = app.buttons["Move"]
+    XCTAssertTrue(moveButton.waitForExistence(timeout: 5))
+    moveButton.tap()
   }
 }
